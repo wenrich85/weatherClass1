4142761315a69a88fb4c569ae3e8206be4587e84
Wendell Richards <wenrich85@gmail.com>
2026-01-15 19:37:00 -0600

    Initial Commit

---
+++ WeatherClass1/WeatherClass1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherClass1App.swift
+//  WeatherClass1
+//
+//  Created by Wendell Richards on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherClass1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
